c7f5cec482f4daeecb39eda071b409da304e6799
stanmak1991 <stanmak1991@gmail.com>
2019-07-29 00:43:16 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CategoryComponent} from './category/category.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'category', component: CategoryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
